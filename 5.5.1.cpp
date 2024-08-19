127d7747af7567a1031f6874e8004cd6290381a3
Kinoko <huiyuanai129@outlook.com>
2024-08-19 13:15:26 +0800

    accumulate(beg, end, value)

---
+++ 5.5.1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<numeric>
+using namespace std;
+
+void test01(){
+
+    vector<int> v;
+    
+    for(int i = 1; i <= 100; i++){
+        v.push_back(i);
+    }
+
+    int total = accumulate(v.begin(), v.end(), 0);
+
+    cout<<total<<endl;
+}
+
+int main(){
+
+    test01();
+
+    return 0;
+}
